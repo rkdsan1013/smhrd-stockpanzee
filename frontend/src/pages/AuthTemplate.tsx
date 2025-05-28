0a95f7f5f2aba130ea93eda38e2d5e164377fcf7
rkdsan1013 <rkdsan1013@gmail.com>
2025-05-28 14:39:54 +0900

    Fix Layout

--- frontend/src/pages/AuthTemplate.tsx
+++ frontend/src/pages/AuthTemplate.tsx
@@ -16,7 +16,7 @@ const AuthTemplate: React.FC = () => {
     <div className="min-h-screen bg-gray-900 text-white">
       <div className="flex flex-col md:flex-row">
         {/* 좌측 영역: 이미지 (md 이상에서 표시) */}
-        <div className="hidden md:block md:w-1/2 pl-4 pr-0 pt-4 pb-4 relative h-screen">
+        <div className="hidden md:block md:w-1/2 pl-4 pr-0 pt-4 pb-4 relative min-h-screen">
           <div
             className="absolute top-4 left-4 bottom-4 right-0 bg-cover bg-center rounded-lg"
             style={{ backgroundImage: "url('/panzee.webp')" }}
@@ -28,7 +28,7 @@ const AuthTemplate: React.FC = () => {
         </div>
 
         {/* 우측 영역: 인증 폼 */}
-        <div className="w-full md:w-1/2 flex flex-col h-screen">
+        <div className="w-full md:w-1/2 flex flex-col min-h-screen">
           {/* 상단 헤더: 로고와 Close 버튼 */}
           <div className="p-6 mx-4 border-b border-gray-700 relative flex items-center justify-center">
             <div className="flex items-center">
@@ -46,29 +46,30 @@ const AuthTemplate: React.FC = () => {
             </button>
           </div>
 
-          {/* 동적 인증 폼 영역 */}
-          <div className="flex-grow flex items-center justify-center px-6 py-8 -mt-4">
+          {/* 중앙 영역: 인증 폼 */}
+          <div className="flex-grow flex items-center justify-center px-6 py-8">
             <div className="w-full max-w-sm">
               <Outlet />
-              {/* 전환 링크: replace 속성을 사용해 현재 기록을 대체 */}
-              <div className="mt-4 text-center text-sm">
-                {isRegister ? (
-                  <span>
-                    이미 계정이 있으신가요?{" "}
-                    <Link to="/auth/login" replace className="text-blue-500">
-                      로그인
-                    </Link>
-                  </span>
-                ) : (
-                  <span>
-                    계정이 없으신가요?{" "}
-                    <Link to="/auth/register" replace className="text-blue-500">
-                      회원가입
-                    </Link>
-                  </span>
-                )}
-              </div>
             </div>
+          </div>
+
+          {/* 하단 전환 링크 영역 - 상단 헤더와 동일한 좌우 여백과 구분선 적용 */}
+          <div className="mx-4 border-t border-gray-700 px-6 py-4 text-center text-base font-medium">
+            {isRegister ? (
+              <span>
+                이미 계정이 있으신가요?{" "}
+                <Link to="/auth/login" replace className="text-blue-500">
+                  로그인
+                </Link>
+              </span>
+            ) : (
+              <span>
+                계정이 없으신가요?{" "}
+                <Link to="/auth/register" replace className="text-blue-500">
+                  회원가입
+                </Link>
+              </span>
+            )}
           </div>
         </div>
       </div>
