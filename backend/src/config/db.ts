2f3ad61baaf1ce35469f658b7a439a0a467b1ef6
rkdsan1013 <rkdsan1013@gmail.com>
2025-05-29 23:48:37 +0900

    Update DB

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,13 +1,15 @@
+// /backend/src/config/db.ts
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
-  database: process.env.DB_NAME || "test_db",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
