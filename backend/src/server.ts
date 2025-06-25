a0b95d2215a8e91c25accfd84cad3b828388d07c
ezpzbb <kimhunwoo0826@gmail.com>
2025-06-25 09:19:44 +0900

    semi

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,6 +8,7 @@ import cookieParser from "cookie-parser";
 import { setupSocket } from "./socket";
 // **경로를 꼭 이대로 유지하세요** (src/services 가 아닌 ./services)
 import { updateCryptoAssetInfoPeriodically } from "./services/binanceService";
+import { startPolygonPriceStream } from "./services/polygonPriceStream";
 
 import authRoutes from "./routes/authRoutes";
 import assetsRoutes from "./routes/assetsRoutes";
@@ -56,7 +57,7 @@ server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 
   // Polygon 스트림
-  // startPolygonPriceStream(io).catch((err) => console.error("Failed to start Polygon stream:", err));
+  startPolygonPriceStream(io).catch((err) => console.error("Failed to start Polygon stream:", err));
 
   // Binance 암호화폐 5초 주기 DB 업데이트
   updateCryptoAssetInfoPeriodically();
