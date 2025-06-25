654aa90cad81f75355c946b626a6f4f2aa682e6b
rkdsan1013 <rkdsan1013@gmail.com>
2025-06-25 15:40:15 +0900

    Update Market

--- frontend/src/components/Icons.tsx
+++ frontend/src/components/Icons.tsx
@@ -99,6 +99,25 @@ const Icons: React.FC<IconProps> = ({ name, className }) => {
         />
       </svg>
     ),
+    angleDown: (
+      <svg
+        className={className || "w-6 h-6 text-gray-800 dark:text-white"}
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="m19 9-7 7-7-7"
+        />
+      </svg>
+    ),
     angleLeft: (
       <svg
         className={className || "w-6 h-6 text-gray-800 dark:text-white"}
@@ -134,6 +153,25 @@ const Icons: React.FC<IconProps> = ({ name, className }) => {
           strokeLinejoin="round"
           strokeWidth="2"
           d="m9 5 7 7-7 7"
+        />
+      </svg>
+    ),
+    angleUp: (
+      <svg
+        className={className || "w-6 h-6 text-gray-800 dark:text-white"}
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="m5 15 7-7 7 7"
         />
       </svg>
     ),
@@ -243,7 +281,6 @@ const Icons: React.FC<IconProps> = ({ name, className }) => {
         />
       </svg>
     ),
-
     eye: (
       <svg
         className={className || "w-6 h-6 text-gray-800 dark:text-white"}
@@ -264,7 +301,6 @@ const Icons: React.FC<IconProps> = ({ name, className }) => {
         <circle cx="12" cy="12" r="3.5" stroke="currentColor" strokeWidth="2" fill="none" />
       </svg>
     ),
-
     userHeadset: (
       <svg
         className={className || "w-6 h-6 text-gray-800 dark:text-white"}
@@ -316,6 +352,59 @@ const Icons: React.FC<IconProps> = ({ name, className }) => {
         <path
           d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
           fill="currentFill"
+        />
+      </svg>
+    ),
+    arrowLeftToBracket: (
+      <svg
+        className={className || "w-6 h-6 text-gray-800 dark:text-white"}
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2"
+        />
+      </svg>
+    ),
+    caretDown: (
+      <svg
+        className={className || "w-6 h-6 text-gray-800 dark:text-white"}
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M18.425 10.271C19.499 8.967 18.57 7 16.88 7H7.12c-1.69 0-2.618 1.967-1.544 3.271l4.881 5.927a2 2 0 0 0 3.088 0l4.88-5.927Z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    ),
+    caretUp: (
+      <svg
+        className={className || "w-6 h-6 text-gray-800 dark:text-white"}
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M5.575 13.729C4.501 15.033 5.43 17 7.12 17h9.762c1.69 0 2.618-1.967 1.544-3.271l-4.881-5.927a2 2 0 0 0-3.088 0l-4.88 5.927Z"
+          clip-rule="evenodd"
         />
       </svg>
     ),
